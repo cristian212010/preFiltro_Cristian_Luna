89d1537295cb18200c8c681e79e4cdafaae58308
cristian212010 <cristiancamilo212010@gmail.com>
2023-10-06 20:17:04 -0500

    feat: :lipstick: SideBar estilos
    se agregaron estilos al sidebar

---
+++ frontend/src/components/sidebar/sidebar.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import logo from '../../assets/logo.png'
+import arrow from '../../assets/arrow.svg';
+import search from '../../assets/search.svg';
+import pronosticoActual from '../../assets/pronosticoActual.svg'
+import city from '../../assets/city.svg';
+import meteor from '../../assets/meteor.svg';
+import { Link } from 'react-router-dom'
+import './sidebar.css'
+
+const SideBar = () => {
+  const [sideBarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!sideBarOpen);
+  }
+
+
+  return (
+    <div>
+      <nav className={`sidebar ${sideBarOpen ? 'close' : ''}`}>
+        <header>
+          <div className="image-text">
+            <span className="image">
+              <img src={logo} alt="" className="logo" />
+            </span>
+            <div className="text logo-text">
+              <span className="name">Tiempo</span>
+              <span className="profession">Artemis</span>
+            </div>
+          </div>
+          <img src={arrow} className='bx bx-chevron-right toggle arrow' onClick={toggleSidebar}></img>
+        </header>
+        <div className="menu-bar">
+          <div className="menu">
+            <li className="search-box">
+              <img src={search} className='bx bx-search search' ></img>
+              <input type="text" placeholder="Search..." />
+            </li>
+            <ul className="menu-links">
+              <Link to='/pronosticoActual'>
+                <li className="nav-link">
+                  <a href="">
+                    <img src={pronosticoActual} className='bx bx-home-alt icon menu-icon'></img>
+                    <span className="text nav-text">Pronostico Actual</span>
+                  </a>
+                </li>
+              </Link>
+              <Link to='/ciudadYPais'>
+                <li className="nav-link">
+                  <a href="">
+                    <img src={city} className='bx bx-home-alt icon menu-icon'></img>
+                    <span className="text nav-text">Ciudad</span>
+                  </a>
+                </li>
+              </Link>
+              <Link to='/alertaMeteorologica'>
+                <li className="nav-link">
+                  <a href="">
+                    <img src={meteor} className='bx bx-home-alt icon'></img>
+                    <span className="text nav-text">Alerta Meteorologica</span>
+                  </a>
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default SideBar;
